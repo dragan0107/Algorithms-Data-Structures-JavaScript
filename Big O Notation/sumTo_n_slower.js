11855d37956dc50a3c9e727ce5c0623a4db1842b
drip016 <73792907+drip016@users.noreply.github.com>
2021-09-08 23:15:18 +0200

    first commit

---
+++ Big O Notation/sumTo_n_slower.js
@@ -0,0 +1,18 @@
+//Write a function that calculates the sum of all numbers from 1 to n (including n)
+
+//for loop slower solution
+
+function addUpTo(n) {
+
+    let sum = 0;
+
+    for (let i = 0; i <= n; i++) {
+
+        sum += i;
+
+    }
+
+    return sum;
+}
+
+console.log(addUpTo(5));
