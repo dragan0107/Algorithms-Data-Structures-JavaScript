3567a56b34541d4d76e7cea4265a38f995921bbe
drip016 <73792907+drip016@users.noreply.github.com>
2021-09-08 23:35:39 +0200

    folder restructuring and new file

---
+++ src/Big O Notation/sumTo_n_faster.js
@@ -0,0 +1,9 @@
+//Write a function that calculates the sum of all numbers from 1 to n (including n)
+
+//faster solution with a formula
+
+function addUpTo(n) {
+    return n * (n + 1) / 2;
+}
+
+console.log(addUpTo(5));
