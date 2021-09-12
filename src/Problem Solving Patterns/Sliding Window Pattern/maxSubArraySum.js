cb7533f41bbbebd722b4d339147a56032ead459f
drip016 <73792907+drip016@users.noreply.github.com>
2021-09-12 12:27:50 +0200

    refactoring the solution

--- src/Problem Solving Patterns/Sliding Window Pattern/maxSubArraySum.js
+++ src/Problem Solving Patterns/Sliding Window Pattern/maxSubArraySum.js
@@ -2,32 +2,31 @@
 The function should calculate the maximum sum of n consecutive elements in the array. */
 
 function maxSubarraySum(arr, n) {
-    let left = 0;
-    let right = n - 1;
 
-    let referenceSum = 0;
+    if (arr.length < n) return null;
 
+    let maxSum = 0;
+    let tempSum = 0;
     for (let i = 0; i < n; i++) {
-        referenceSum += arr[i];
+        maxSum += arr[i];
     }
 
-    for (let i = 0; i < arr.length; i++) {
-        let tempSum = referenceSum - arr[i] + arr[right + 1];
+    tempSum = maxSum;
 
-        if (tempSum > referenceSum) {
-            referenceSum = tempSum;
+    for (let i = n; i < arr.length; i++) {
+        tempSum = tempSum - arr[i - n] + arr[i];
+
+        if (tempSum > maxSum) {
+            maxSum = tempSum;
         }
-        left++;
-        right++;
+
     }
-    console.log(referenceSum);
 
-    return referenceSum;
-
+    return maxSum;
 }
 
-console.log(maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 3)) // 10
-    // console.log(maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 4)) // 17
-    // maxSubarraySum([4, 2, 1, 6], 1) // 6
-    // maxSubarraySum([4, 2, 1, 6, 2], 4) // 13
-    // maxSubarraySum([], 4) // null
+console.log(maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 2)) // 10
+console.log(maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 4)) // 17
+maxSubarraySum([4, 2, 1, 6], 1) // 6
+maxSubarraySum([4, 2, 1, 6, 2], 4) // 13
+maxSubarraySum([], 4) // null
