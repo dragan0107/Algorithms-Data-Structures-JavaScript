7d1d7d397cc1f445aa409275421578d45f0a135c
drip016 <73792907+drip016@users.noreply.github.com>
2021-09-21 01:51:37 +0200

    sorting algorithms added

---
+++ src/Sorting Algorithms/bubbleSort.js
@@ -0,0 +1,29 @@
+/* Start looping from with a variable called i the end of the array towards the beginning
+Start an inner loop with a variable called j from the beginning until i - 1
+If arr[j] is greater than arr[j+1], swap those two values!
+Return the sorted array */
+
+
+function swap(arr, idx1, idx2) {
+    let temp = arr[idx1];
+    arr[idx1] = arr[idx2];
+    arr[idx2] = temp;
+}
+
+
+const bubbleSort = (arr) => {
+
+    for (let i = arr.length - 1; i > 0; i--) {
+        for (let j = 0; j < i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr, j, j + 1)
+            }
+        }
+
+    }
+
+    return arr;
+
+}
+
+console.log(bubbleSort([5, 76, 3, 6, 8, 9, 10, 2, 1, 17, 55]));
