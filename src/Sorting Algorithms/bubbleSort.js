63278338a9b417ec8d550ccdbcc340ff2ae9a098
drip016 <73792907+drip016@users.noreply.github.com>
2021-09-22 00:23:52 +0200

    refactoring

--- src/Sorting Algorithms/bubbleSort.js
+++ src/Sorting Algorithms/bubbleSort.js
@@ -4,7 +4,7 @@ If arr[j] is greater than arr[j+1], swap those two values!
 Return the sorted array */
 
 
-const swap = (arr, idx1, idx2) => {
+const swap = (arr, idx1, idx2) => { // short function for element swapping
     let temp = arr[idx1];
     arr[idx1] = arr[idx2];
     arr[idx2] = temp;
@@ -13,12 +13,15 @@ const swap = (arr, idx1, idx2) => {
 
 const bubbleSort = (arr) => {
 
-    for (let i = arr.length - 1; i > 0; i--) {
+    for (let i = arr.length - 1; i > 0; i--) { // Outer loop is helping us shrink the window we're sorting, to speed up the process, as the highest number will always be pushed to the end.
+        let noSwap = true; // we use this variable to check if there was a swap being done or not. if not, it stays true so we break out of the loop.
         for (let j = 0; j < i; j++) {
             if (arr[j] > arr[j + 1]) {
-                swap(arr, j, j + 1)
+                swap(arr, j, j + 1);
+                noSwap = false; // noSwap turned to false since the swap has been done.
             }
         }
+        if (noSwap) break; // we break out of the loop, since no swap was done which means its already sorted.
     }
     return arr;
 }
