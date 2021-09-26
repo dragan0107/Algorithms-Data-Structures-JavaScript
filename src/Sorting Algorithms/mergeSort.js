ba388717b4ff97025ead3d30345f2133fa2ceb01
drip016 <73792907+drip016@users.noreply.github.com>
2021-09-27 00:58:53 +0200

    merge sort helper added - first part

--- src/Sorting Algorithms/mergeSort.js
+++ src/Sorting Algorithms/mergeSort.js
@@ -4,10 +4,37 @@ If the value in the first array is smaller than the value in the second array, p
 If the value in the first array is larger than the value in the second array, push the value in the second array into our results and move on to the next value in the second array
 Once we exhaust one array, push in all remaining values from the other array */
 
-function mergeHelper(arr1, arr2) {
+function merge(arr1, arr2) { // We use this function to merge 2 sorted arrays back to one and return it.
 
+    let result = [];
+    let i = 0;
+    let j = 0;
 
+    while (i < arr1.length && j < arr2.length) {
+        if (arr1[i] < arr2[j]) {
+            result.push(arr1[i]);
+            i++;
+        } else if (arr2[j] < arr1[i]) {
+            result.push(arr2[j]);
+            j++
+        } else {
+            result.push(arr1[i])
+            i++;
+            j++;
+        }
+    }
 
+    while (i < arr1.length) {
+        result.push(arr1[i]);
+        i++;
+    }
+    while (j < arr2.length) {
+        result.push(arr2[j]);
+        j++;
+    }
 
     return result;
 }
+
+
+console.log(mergeHelper([1, 6, 8, 24, 55, 101], [2, 5, 9, 10, 12, 15]));
