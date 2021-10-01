6d8b79bfcae294322631954df0c9ffaca43d9016
drip016 <73792907+drip016@users.noreply.github.com>
2021-10-02 00:57:33 +0200

    radix sort finished

--- src/Sorting Algorithms/radixSort.js
+++ src/Sorting Algorithms/radixSort.js
@@ -1,17 +1,12 @@
-// getDigit helper function that returns the position index of the input value.
+/* Radix sort creates 10 containers or buckets in a form of an array, for each digit from 0 - 9;
+We figure out the number with longest digits in the array. That's how many times outer loop will re-sort the numbers.
+Outer loop checks what digit is on k position of a number, and puts it in its corresponding bucket. 
+After we do it with each number of the input array, we use spread operator to reassign our input array to this new array
+After the outer loop finishes, all numbers are sorted properly */
 
-// function getDigit(number, place) {
-//     let value = String(Math.abs(number));
-//     console.log(value);
-//     let reversedValue = '';
-//     for (let i = value.length - 1; i >= 0; i--) {
-//         reversedValue += value[i];
-//     }
-//     if (place > value.length - 1) return 0;
-//     return reversedValue[place];
-// }
 
-// Shorter version that does exactly the same as the one above.
+
+// getDigit helper function that returns the value of the digit at the i position.
 function getDigit(num, i) {
     return Math.floor(Math.abs(num) / Math.pow(10, i)) % 10;
 }
@@ -30,3 +25,34 @@ function mostDigits(nums) {
     }
     return maxDigits;
 }
+
+
+function radixSort(arr) {
+    let maxDigits = mostDigits(arr);
+    for (let k = 0; k < maxDigits; k++) {
+        let digitBoxes = Array.from({ length: 10 }, () => []);
+        for (let i = 0; i < arr.length; i++) {
+            let currentDigit = getDigit(arr[i], k);
+            digitBoxes[currentDigit].push(arr[i]);
+        }
+        arr = [].concat(...digitBoxes);
+        console.log(arr);
+    }
+    return arr;
+}
+
+
+console.log(radixSort([1552, 33435, 255, 54, 11, 4, 1717, 14, 9953]));
+
+
+
+// function getDigit(number, place) {
+//     let value = String(Math.abs(number));
+//     console.log(value);
+//     let reversedValue = '';
+//     for (let i = value.length - 1; i >= 0; i--) {
+//         reversedValue += value[i];
+//     }
+//     if (place > value.length - 1) return 0;
+//     return reversedValue[place];
+// }
