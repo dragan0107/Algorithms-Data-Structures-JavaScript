6f3002946ac3218ccb2a186e26f7cc578b4ad121
drip016 <73792907+drip016@users.noreply.github.com>
2021-10-17 23:33:56 +0200

    dll unshift method

--- src/Data Structures/Doubly Linked Lists/doublyLinkedLists.js
+++ src/Data Structures/Doubly Linked Lists/doublyLinkedLists.js
@@ -1,4 +1,4 @@
-class Node {
+class Node { // Dll node class.
     constructor(val) {
         this.val = val;
         this.next = null;
@@ -12,32 +12,46 @@ class DoublyLinkedList {
         this.tail = null;
         this.length = 0;
     }
-    push(val) {
+    push(val) { // Push method adds one node at the end of the list.
         let newNode = new Node(val);
-        if (this.length === 0) {
+        if (this.length === 0) { // If list is empty, we set new node to be head and tail.
             this.head = newNode;
             this.tail = this.head;
         } else {
-            let oldTail = this.tail;
-            this.tail.next = newNode;
-            newNode.previous = oldTail;
-            this.tail = newNode;
+            let oldTail = this.tail; // Firstly we store old tail into a variable.
+            this.tail.next = newNode; // Then we change its next value to point at the new node.
+            newNode.previous = oldTail; // Now, we point to the old tail from the new node.
+            this.tail = newNode; // And we finally set the new tail to be new node.
         }
-        this.length++;
+        this.length++; // Length increase by 1.
     }
-    pop() {
-        let currentTail = this.tail;
+    pop() { // Pop method removes one node from the end.
+        let oldTail = this.tail; // Storing current tail.
         if (!this.tail) return undefined;
-        if (this.length === 1) {
+        if (this.length === 1) { // If only one node in the list, we set both head and tail to null.
             this.head = null;
             this.tail = null;
         } else {
-            this.tail = currentTail.previous;
-            this.tail.next = null;
-            currentTail.previous = null;
+            this.tail = oldTail.previous; // We re-define new tail to be the old tail's previous value.
+            this.tail.next = null; // And we set the new tails next value to be null.
+            oldTail.previous = null; // We also set old tail's prev value to be null, so we don't have access to it when we return.
         }
         this.length--;
-        return currentTail;
+        return oldTail;
+    }
+    shift() {
+        let oldHead = this.head;
+        if (!this.head) return undefined;
+        if (this.length === 1) { // If only one node in the list, we set both head and tail to null.
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.head = oldHead.next;
+            this.head.previous = null;
+            oldHead.next = null;
+        }
+        this.length--;
+        return oldHead;
     }
 }
 
@@ -47,5 +61,6 @@ list.push(14);
 list.push(17);
 list.push(44);
 list.push(76);
-console.log(list.pop());
+console.log(list.shift());
+
 console.log(list);
