b67c77241dcc101019a15cfa0a207683ec89fe55
drip016 <73792907+drip016@users.noreply.github.com>
2021-10-22 01:32:11 +0200

    BST insert method implemented.

--- src/Data Structures/Binary Search Trees/BinarySearchTree.js
+++ src/Data Structures/Binary Search Trees/BinarySearchTree.js
@@ -14,15 +14,38 @@ class BinarySearchTree {
         let newNode = new Node(val);
         if (!this.root) {
             this.root = newNode;
+            return this;
         } else {
-            if (val > this.root.val) {
-                if (this.root.right) {
-
-                } else {
-
+            let current = this.root;
+            while (true) {
+                if (val > current.val) {
+                    if (current.right) {
+                        current = current.right;
+                    } else {
+                        current.right = newNode;
+                        return this;
+                    }
+                } else if (val < current.val) {
+                    if (current.left) {
+                        current = current.left;
+                    } else {
+                        current.left = newNode;
+                        return this;
+                    }
                 }
             }
         }
 
     }
 }
+
+let bst = new BinarySearchTree();
+
+bst.insert(10)
+bst.insert(13)
+bst.insert(16)
+bst.insert(11)
+bst.insert(5)
+bst.insert(7)
+bst.insert(2)
+console.log(bst);
