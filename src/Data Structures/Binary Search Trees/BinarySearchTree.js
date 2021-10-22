0e4108d21ed298bc95b0058252d9f98201436c11
drip016 <73792907+drip016@users.noreply.github.com>
2021-10-22 23:29:30 +0200

    commented BST out

--- src/Data Structures/Binary Search Trees/BinarySearchTree.js
+++ src/Data Structures/Binary Search Trees/BinarySearchTree.js
@@ -1,4 +1,4 @@
-class Node {
+class Node { // Each node of BST has a value, pointer to the left and pointer to the right.
     constructor(val) {
         this.val = val;
         this.left = null;
@@ -10,25 +10,25 @@ class BinarySearchTree {
     constructor() {
         this.root = null;
     }
-    insert(val) {
+    insert(val) { // Tree insertion method.
         let newNode = new Node(val);
-        if (!this.root) {
+        if (!this.root) { // If the tree is empty, there's no root so therefore we initiate it.
             this.root = newNode;
             return this;
-        } else {
-            let current = this.root;
-            while (true) {
+        } else { // If not empty we have to find where to store the node.
+            let current = this.root; // We define the variable current which will be updated with every while loop iteration.
+            while (true) { // Loop will be running until we break out with return statement.
                 if (val > current.val) {
-                    if (current.right) {
+                    if (current.right) { // If input value is greater than current, and there is a value on current's right, we update current.
                         current = current.right;
-                    } else {
+                    } else { // If not, we store the node, and return to break out of loop.
                         current.right = newNode;
                         return this;
                     }
-                } else if (val < current.val) {
-                    if (current.left) {
+                } else if (val < current.val) { // If value is lower than current's
+                    if (current.left) { // If value is lower than current's,  but there is a value on current's left, we update.
                         current = current.left;
-                    } else {
+                    } else { // If there is no value on current's left, we store the new node there and return to break out of loop.
                         current.left = newNode;
                         return this;
                     }
