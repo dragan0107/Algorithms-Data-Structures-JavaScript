a3d959c7cd5f5250d8eb0f4281c5b3228506277d
Dragan Stojicic <dragan.stojicic@ingsoftware.com>
2024-01-29 09:39:55 +0100

    add Graph class and traversing methods

---
+++ src/Data Structures/Graphs/undirectedGraph.js
@@ -0,0 +1,66 @@
+class Graph {
+    constructor() {
+        this.adjacencyList = {};
+    }
+    addVertex(vertex) {
+        if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+    }
+    addEdge (vertex1, vertex2) {
+        this.adjacencyList[vertex1].push(vertex2);
+        this.adjacencyList[vertex2].push(vertex1);
+    }
+    removeEdge(vertex1, vertex2) {
+        this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+            v => v !== vertex2
+        )
+        this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+            v => v !== vertex1
+        )
+    }
+    removeVertex(vertex) {
+        this.adjacencyList[vertex].forEach(v=> {
+            this.removeEdge(v, vertex)
+        })
+        delete this.adjacencyList[vertex]
+    }
+
+    DFSRecursive(vertex) {
+        let visited = {}
+        let result = []
+
+        let DFS = (vertex) => {
+            if (!vertex) return null;
+            visited[vertex] = 'true';
+            result.push(vertex);
+            this.adjacencyList[vertex].forEach(v => {
+                console.log(v)
+                if(!visited[v]) DFS(v)
+            })
+        }
+
+        DFS(vertex)
+        return result;
+    }
+}
+
+let g = new Graph();
+g.addVertex('A');
+g.addVertex('B');
+g.addVertex('C');
+g.addVertex('D');
+g.addVertex('E');
+g.addVertex('F');
+
+
+g.addEdge('A', 'B');
+g.addEdge('A', 'C');
+g.addEdge('B', 'D');
+g.addEdge('C', 'E');
+g.addEdge('D', 'E');
+g.addEdge('D', 'F');
+g.addEdge('E', 'F');
+
+console.log(g['adjacencyList']);
+
+const res = g.DFSRecursive('A')
+console.log(res)
